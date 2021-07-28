38f2a02ad97cd9b81f432bfe6079544ea5ee3482
VivekMuraleedharan <vivekmuraleedharan73@gmail.com>
2021-07-28 10:55:49 +0530

    add git_data

--- src/get_data.py
+++ src/get_data.py
@@ -15,8 +15,10 @@ def read_params(config_path):
 
 def get_data(config_path):
     config= read_params(config_path)
-    print(config)
-   # data_path = config[]
+    #print(config)
+    data_path = config["data_source"]["s3_source"]
+    df= pd.read_csv(data_path,sep= ",",encoding='utf-8')
+    return df
 
 if __name__ =="__main__":
     args = argparse.ArgumentParser()
