ce7bf4c72158c75991a3f352a39eb12707b8db9c
VivekMuraleedharan <vivekmuraleedharan73@gmail.com>
2021-07-30 12:01:28 +0530

    added the tox file

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup,find_packages
+
+setup(
+      name= "src",
+      version = "0.0.1",
+      description = "its a test package",
+      author = "vivek",
+      packages = find_packages(),
+      license = "MIT"
+      
+      
+      )
---
+++ tests/test_config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+def test_generic():
+    a= 2
+    b= 2
+    assert a==b
